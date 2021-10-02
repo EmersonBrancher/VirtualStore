b2bbfe728fabf9a764a837da9486b9cfdce94672
Haku <brancher81@gmail.com>
2021-10-02 00:55:27 -0300

    Added title and description of product

--- virtual_store/lib/screens/product_screen.dart
+++ virtual_store/lib/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:virtual_store/datas/product_data.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -21,29 +22,61 @@ class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(114, 95, 105, 100),
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: Text(product.title),
         centerTitle: true,
       ),
       body: ListView(
         children: [
-          CarouselSlider(
-            options: CarouselOptions(
-              autoPlay: false,
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                enableInfiniteScroll: false,
+                autoPlay: false,
+                aspectRatio: 1,
+              ),
+              items: product.images.toList().map((url) => ClipRRect(
+                borderRadius: BorderRadius.circular(60),
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      url,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+              )).toList(),
             ),
-            items: product.images.toList().map((e) => ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Stack(
-                fit: StackFit.expand,
+          ),
+          Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Image.network(e,
-                  width: 100,
-                    height: 350,
-                    fit: BoxFit.cover,
+                  Text(
+                    product.title,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 3,
                   ),
+                  Text(
+                    "R\$${product.price.toStringAsFixed(2)}",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(),
                 ],
               ),
-            )).toList(),
           ),
         ]
       ),
